d508217122b021697f944f098bd8a10589be437f
shafiqns <115532625+shafiqns@users.noreply.github.com>
2023-01-26 22:44:11 +0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "../../App.css";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
@@ -6,24 +6,40 @@ function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
 
+  const InputHandler = (event) => {
+    setText(event.target.value);
+    if (event.target.value === "Ready!") {
+      setIsReady(true);
+    }
+    if (event.target.value !== "Ready!"){
+      setIsReady(false)
+    }
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
+        <NavLink to="/auth">
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </NavLink>
         <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
         </b>
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <input type="text" name="name" onChange={InputHandler} value={text} />
+        {isReady ? (
+          ""
+        ) : (
+          <span style={{ color: "red" }}>I am not ready yet!</span>
+        )}
       </header>
     </div>
   );
